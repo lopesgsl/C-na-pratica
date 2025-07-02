a17485f827499ed6a506e010d7aaf9e62bae8494
lopesgsl <lopessilvagustavo20@gmail.com>
2025-07-02 01:02:51 -0300

    Fixed bug logout

--- script/auth.js
+++ script/auth.js
@@ -1,6 +1,6 @@
 function auth_login(){
     
-    if (localStorage.getItem('usuario_atual') == null) {
+    if (localStorage.getItem('usuario_atual') == 'null' || localStorage.getItem('usuario_atual') == null) {
         // Se o usuário não estiver logado, redireciona para a página de login
         window.location.href = 'login.html';
     }
--- script/logout.js
+++ script/logout.js
@@ -4,9 +4,7 @@ const logoutButton = document.getElementById('logout');
 // Função para realizar o logout
 function logout() {
     // Define o usuario_atual como null (pode ser em localStorage, sessionStorage ou variável global)
-    localStorage.removeItem('usuario_atual');
-    let usuario_atual = localStorage.getItem("usuario_atual") ? JSON.parse(localStorage.getItem("usuario_atual")) : null;
- // Remove o usuário atual do localStorage
+    localStorage.setItem('usuario_atual', null);
     console.log('Usuário deslogado com sucesso.');
     // Redireciona para a página de login ou inicial, se necessário
     window.location.href = 'index.html';
